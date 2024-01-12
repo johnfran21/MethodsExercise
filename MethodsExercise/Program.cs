f28881bfa0226f78bf855d1671780b89e9cfebce
John Franzese <johnfranzese21@gmail.com>
2024-01-11 20:00:25 -0600

    Method Exercise 1 Update

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Madlib();
+        }
+
+        public static void Madlib()
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}, What is your favorite Sport?");
+            string favSport = Console.ReadLine();
+            Console.WriteLine("What is your favorite Food?");
+            string favFood = Console.ReadLine();
+            Console.WriteLine("What is your favorite Season?");
+            string favSeason = Console.ReadLine();
+
+            Console.WriteLine($"One day {userName} was playing {favSport} outside during the {favSeason} and after hours of playing he finally sat down to eat at {favFood}");
         }
     }
 }
