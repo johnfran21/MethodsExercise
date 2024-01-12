6b68eae339fd0034911cfc09f339001305113d1e
John Franzese <johnfranzese21@gmail.com>
2024-01-11 21:06:49 -0600

    Method Exercise Update 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Madlib();
+            //Madlib();
+            int added = Add(2, 4);
+            Console.WriteLine(added);
+
+            int removed = Subtract(14, 2);
+            Console.WriteLine(removed);
+
+            int multiplied = Multiply(6, 4);
+            Console.WriteLine(multiplied);
+
+            int divided = Divide(18, 2);
+            Console.WriteLine(divided);
+
+            int sum = AddAllSums(1, 1, 1, 1, 1);
+            Console.WriteLine(sum);
+
+
         }
 
         public static void Madlib()
@@ -20,5 +36,48 @@
 
             Console.WriteLine($"One day {userName} was playing {favSport} outside during the {favSeason} and after hours of playing he finally sat down to eat at {favFood}");
         }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+
+        }
+
+        public static int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+
+        }
+
+        public static int AddAllSums(params int[] nums)
+        {
+            int sum = 0;
+            foreach (int number in nums)
+            {
+                sum = sum + number;
+            }
+
+            return sum;
+        }
+
+
     }
 }
